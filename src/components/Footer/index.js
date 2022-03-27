44be36a98b1a736c3eca909ad73b59fae8ea5b6c
CraigJensen <cmjensen82@gmail.com>
2022-03-27 11:45:31 -0500

    footer finished, added images and links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import github from '../../images/github.svg';
+import linkedIn from '../../images/linkedIn.svg';
+import stackOverflow from '../../images/stackOverflow.svg';
 
 function Footer() {
-  return <footer className="App-footer">footer</footer>;
+  return (
+    <footer className="App-footer">
+      <div className="icons">
+        <a
+          href="https://github.com/CraigMJensen?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={github} alt="Github" />
+        </a>
+      </div>
+      <div className="icons">
+        <a
+          href="https://www.linkedin.com/in/craig-jensen-b6594594/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedIn} alt="LinkedIn" />
+        </a>
+      </div>
+      <div className="icons">
+        <a
+          href="https://stackoverflow.com/users/16736205/craig-jensen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={stackOverflow} alt="Stack Overflow" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
