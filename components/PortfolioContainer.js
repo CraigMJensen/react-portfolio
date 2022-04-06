c663103b7f0d2c27253c8346db6f704f0ae576a5
CraigJensen <cmjensen82@gmail.com>
2022-04-06 18:48:17 -0500

    Updates

---
+++ components/PortfolioContainer.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Footer from '../components/Footer';
+import Nav from './Nav';
+
+
+function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+  return (
+    <div>
+      <div className="App">
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      </div>
+      <div className="App-body">
+        {renderPage()}
+      </div>
+      <div className='App-footer'>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioContainer;
---
+++ components/pages/Portfolio/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Project from '../../Project';
+
+
+
+function Portfolio() {
+  
+  
+  
+  return (
+    <div className="portfolio-page wrapper">
+      <div>
+        <h2 className="title">Portfolio</h2>
+      </div>
+      <div className="project-container">
+      
+          <Project />
+
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
