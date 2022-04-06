6b30c3a74aa8adfa11a3dfb86118ea678e852f49
CraigJensen <cmjensen82@gmail.com>
2022-04-06 18:55:15 -0500

    fixing a rendering issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-
-=======
 import React from 'react';
->>>>>>> develop
 import './App.css';
 import PortfolioContainer from './components/PortfolioContainer';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-       
-      </header>
-    </div>
-  );
-=======
   return <PortfolioContainer />;
->>>>>>> develop
 }
 
 export default App;
