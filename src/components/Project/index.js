8007f1511d8b059938459d21a051fa5cef46c1ca
craigmjensen <cmjensen82@gmail.com>
2022-04-08 17:17:05 -0500

    final styling for portfolio project page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,27 +4,41 @@ import githubSvg from '../../assets/images/github.svg';
 
 function Project() {
   return (
-    <div className='row'>
-    
+    <div className="row">
       {projectData.map((data, i) => (
         <div className="column" key={i}>
           <div className="card">
             <div className="image-container">
-              <a href={data.deployed} target='_blank' rel='noreferrer' >
-                <img className='project-image' src={data.image} alt={data.description} />
+              <a href={data.deployed} target="_blank" rel="noreferrer">
+                <img
+                  className="project-image"
+                  src={data.image}
+                  alt={data.description}
+                />
               </a>
             </div>
             <div className="content">
-              <a href={data.github} title={data.title} target='_blank' rel='noreferrer'>
-                <img className='gitIcon' src={githubSvg} alt="GitHub Repo" />
+              <a
+                href={data.github}
+                title="Github repo link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="gitIcon" src={githubSvg} alt="GitHub Repo" />
+              </a>
+              <a
+                href={data.deployed}
+                title={data.title}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <h3 className="card-title">{data.title}</h3>
               </a>
             </div>
           </div>
         </div>
       ))}
-      
-      </div>
+    </div>
   );
 }
 
