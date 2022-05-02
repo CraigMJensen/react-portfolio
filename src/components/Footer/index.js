1c0795ca98a9c922e9da6e187f63817eab247f97
CraigJensen <cmjensen82@gmail.com>
2022-05-02 18:31:02 -0500

    refactored sass, added media queries in correct spots

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,33 +6,35 @@ import stackOverflow from '../../assets/images/stackOverflow.svg';
 function Footer() {
   return (
     <footer className="App-footer">
-      <div className="icons">
-        <a
-          href="https://github.com/CraigMJensen?tab=repositories"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <a
+        href="https://github.com/CraigMJensen?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="icons">
           <img src={github} alt="Github" />
-        </a>
-      </div>
-      <div className="icons">
-        <a
-          href="https://www.linkedin.com/in/craig-jensen-b6594594/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/craig-jensen-b6594594/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="icons">
           <img src={linkedIn} alt="LinkedIn" />
-        </a>
-      </div>
-      <div className="icons">
-        <a
-          href="https://stackoverflow.com/users/16736205/craig-jensen"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+      </a>
+
+      <a
+        href="https://stackoverflow.com/users/16736205/craig-jensen"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="icons">
           <img src={stackOverflow} alt="Stack Overflow" />
-        </a>
-      </div>
+        </div>
+      </a>
     </footer>
   );
 }
